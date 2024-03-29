77a5f21a8c18ad4ebaa41a003b5539c4365a9502
gamejoye <gamejoye@gmail.com>
2024-03-29 22:19:06 +0800

    feat(modules): new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { EnvConfigModule } from './modules/env-config/env-config.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ChatroomsModule } from './modules/chatrooms/chatrooms.module';
+import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [
+    EnvConfigModule,
+    UsersModule,
+    AuthModule,
+    ChatroomsModule,
+    MessagesModule,
+  ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) { }
-}
+export class AppModule {}
