9beb889355dc1382c4e9f13471ed1decee502ee5
gamejoye <gamejoye@gmail.com>
2024-04-09 22:22:00 +0800

    feat(module): 导入user模块

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     PassportModule.register({
       defaultStrategy: ['jwt', 'local'],
     }),
+    UsersModule,
   ],
   providers: [LocalStrategy, JwtStrategy],
   controllers: [AuthController],
