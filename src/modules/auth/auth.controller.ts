b1d4523f575ffc3147f4ef3c7f145d63bf255175
gamejoye <gamejoye@gmail.com>
2024-04-09 22:21:45 +0800

    feat(controller): 登录和注册endpoint逻辑实现

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,29 @@
-import { Body, Controller, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LocalGuard } from './local.guard';
 import { GetUser } from './decorators/get-user.decorator';
 import { EnvConfigService } from '../env-config/env-config.service';
 import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
 import { User } from '../users/entities/user.entity';
 import { LoginUserRequestDto } from './dto/login.dto';
 import { RegisterUserRequestDto } from './dto/register.dto';
+import { UsersService } from '../users/users.service';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private envConfigService: EnvConfigService) {}
+  constructor(
+    private envConfigService: EnvConfigService,
+    private usersService: UsersService,
+  ) {}
 
   @Post('login')
   @UseGuards(LocalGuard)
@@ -37,8 +49,21 @@ export class AuthController {
   @ApiBody({ type: RegisterUserRequestDto })
   @ApiOperation({ summary: '用户注册' })
   @ApiResponse({ status: HttpStatus.OK, description: '注册成功' })
+  @ApiResponse({ status: HttpStatus.CONFLICT, description: '用户邮箱已经存在' })
   @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '注册失败' })
   async register(@Body() registerDto: RegisterUserRequestDto) {
-    return "";
+    const { email, password, username, avatarUrl } = registerDto;
+    const existing = await this.usersService.getByEmail(email);
+    if (existing) {
+      throw new HttpException('email already exists', HttpStatus.CONFLICT);
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await this.usersService.register(
+      username,
+      email,
+      passwordHash,
+      avatarUrl,
+    );
+    return user;
   }
 }
