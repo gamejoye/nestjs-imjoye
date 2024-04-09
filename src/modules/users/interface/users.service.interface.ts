555091d75e597570086b80a59559c0a2692c031d
gamejoye <gamejoye@gmail.com>
2024-04-09 22:16:58 +0800

    feat(service): 用户增查

---
+++ src/modules/users/interface/users.service.interface.ts
@@ -0,0 +1,12 @@
+import { User } from '../entities/user.entity';
+
+export interface IUsersService {
+  getByEmail(email: string): Promise<User | null>;
+  getById(id: number): Promise<User | null>;
+  register(
+    username: string,
+    email: string,
+    passwordHash: string,
+    avatarUrl: string,
+  ): Promise<User>;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { IUsersService } from './interface/users.service.interface';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { USER_REPOSITORY } from 'src/common/constants/providers';
 
 @Injectable()
-export class UsersService {}
+export class UsersService implements IUsersService {
+  constructor(
+    @Inject(USER_REPOSITORY)
+    protected usersRepository: Repository<User>,
+  ) {}
+  async getById(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    return user;
+  }
+  async register(
+    username: string,
+    email: string,
+    passwordHash: string,
+    avatarUrl: string,
+  ): Promise<User> {
+    const partial = this.usersRepository.create({
+      username,
+      email,
+      passwordHash,
+      avatarUrl,
+    });
+    const user = await this.usersRepository.save(partial);
+    return user;
+  }
+  async getByEmail(email: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+}
