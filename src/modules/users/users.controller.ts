a8f0293cf535386014a349bc6d2c5c38253c2fa4
gamejoye <gamejoye@gmail.com>
2024-04-10 23:58:08 +0800

    feat(controller): 根据userid获取用户信息

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from '../auth/jwt.guard';
+import { UsersService } from './users.service';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  @Get()
-  async getUserByToken() {
-    return '';
+  constructor(protected readonly usersService: UsersService) {}
+  @Get(':id')
+  @UseGuards(JwtGuard)
+  async getUserByid(@GetUser() user: User, @Param('id') id: number) {
+    if (user.id !== id) {
+      throw new HttpException('invaid token', HttpStatus.FORBIDDEN);
+    }
+    return user;
   }
 }
