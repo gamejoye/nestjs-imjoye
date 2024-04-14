39f1abbd9a5e7efabdbe2dee0f273f2e6cb0febb
gamejoye <gamejoye@gmail.com>
2024-04-14 23:55:14 +0800

    feat: USER_FRIENDSHIP_REPOSITORY provider

--- src/modules/users/users.providers.ts
+++ src/modules/users/users.providers.ts
@@ -1,11 +1,22 @@
-import { DATA_SOURCE, USER_REPOSITORY } from 'src/common/constants/providers';
+import {
+  DATA_SOURCE,
+  USER_FRIENDSHIP_REPOSITORY,
+  USER_REPOSITORY,
+} from 'src/common/constants/providers';
 import { User } from './entities/user.entity';
 import { DataSource } from 'typeorm';
+import { UserFriendship } from './entities/friendship.entity';
 
 export const usersProviders = [
   {
     provide: USER_REPOSITORY,
     useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
+    inject: [DATA_SOURCE],
+  },
+  {
+    provide: USER_FRIENDSHIP_REPOSITORY,
+    useFactory: (dataSource: DataSource) =>
+      dataSource.getRepository(UserFriendship),
     inject: [DATA_SOURCE],
   },
 ];
