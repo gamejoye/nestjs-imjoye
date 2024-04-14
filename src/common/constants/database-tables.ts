4c8b5fbe026087722d0cca97e7612354f14c5fd6
gamejoye <gamejoye@gmail.com>
2024-04-14 23:51:43 +0800

    feat(constant): 好友映射表名

--- src/common/constants/database-tables.ts
+++ src/common/constants/database-tables.ts
@@ -3,3 +3,4 @@ export const MESSAGE = 'message';                     // 消息
 export const USER = 'user';                           // 用户
 export const CHATROOM = 'chatroom';                   // 聊天室
 export const USER_CHATROOM = 'user_chatroom';         // 用户聊天室映射表
+export const USER_FRIENDSHIP = 'user_friendship';     // 用户好友映射表
