4c6c57b5e194c03efb639599a6c89c581cf4ba71
gamejoye <gamejoye@gmail.com>
2024-04-14 15:55:33 +0800

    perf: use logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { EnvConfigModule } from './modules/env-config/env-config.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatroomsModule } from './modules/chatrooms/chatrooms.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { WsGatewayModule } from './modules/ws-gateway/ws-gateway.module';
+import { LoggerMiddleWare } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -16,4 +17,10 @@ import { WsGatewayModule } from './modules/ws-gateway/ws-gateway.module';
     WsGatewayModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleWare)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
