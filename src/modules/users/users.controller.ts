0a0624af0425153a34afa4e17d2a1686d0107f4b
gamejoye <gamejoye@gmail.com>
2024-04-20 20:55:56 +0800

    feat(controller): 处理头像上传

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -4,19 +4,29 @@ import {
   HttpException,
   HttpStatus,
   Param,
+  Post,
   Query,
+  Res,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from '../auth/jwt.guard';
 import { UsersService } from './users.service';
 import { GetUser } from '../auth/decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { EnvConfigService } from '../env-config/env-config.service';
+import { Response } from 'express';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(protected readonly usersService: UsersService) {}
+  constructor(
+    protected readonly usersService: UsersService,
+    protected readonly envConfigService: EnvConfigService,
+  ) { }
   @Get(':id')
   @UseGuards(JwtGuard)
   async getUserByid(@GetUser() user: User, @Param('id') id: number) {
@@ -52,5 +62,17 @@ export class UsersController {
       );
       return friends;
     }
+  }
+  @Post('avatar/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const filename = file.filename;
+    const avatarUrl = this.envConfigService.getAvatarConfig().avatarUrl;
+    const userAvatarUrl = avatarUrl + '/' + filename;
+    return userAvatarUrl;
   }
 }
