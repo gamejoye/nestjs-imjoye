dd3424d981525d68c6ac5a3839b6fbb761d4a167
gamejoye <gamejoye@gmail.com>
2024-04-20 20:57:43 +0800

    refactor(interceptor): 对于头像上传的返回值特别情况处理

--- src/common/interceptors/res-transform.interceptors.ts
+++ src/common/interceptors/res-transform.interceptors.ts
@@ -8,6 +8,7 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IResponse } from '../types/response.type';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class ResTransformInterceptor<T>
@@ -17,13 +18,21 @@ export class ResTransformInterceptor<T>
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<IResponse<T>> {
+    const response: Response = context.switchToHttp().getResponse();
+    const request: Request = context.switchToHttp().getRequest();
     return next.handle().pipe(
-      map((data: T) => ({
-        statusCode: context.switchToHttp().getResponse()
-          .statusCode as HttpStatus,
-        data: data,
-        message: 'success',
-      })),
+      map((data: T) => {
+        const transformed = {
+          statusCode: response.statusCode as HttpStatus,
+          data: data,
+          message: 'success',
+        };
+        // 鸿蒙提供的request.uploadFile 无法获取后端的response 所以这里折中方案在response.headers提供返回值
+        if (request.path === '/users/avatar/upload') {
+          response.setHeader('status', JSON.stringify(transformed));
+        }
+        return transformed;
+      }),
     );
   }
 }
