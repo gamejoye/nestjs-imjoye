2e9bc4c5aaec6394208dd1de8898fd347586c603
gamejoye <gamejoye@gmail.com>
2024-04-20 20:54:44 +0800

    feat(module): 集成MulterFile模块

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,32 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from '../database/database.module';
 import { usersProviders } from './users.providers';
+import { MulterModule } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { EnvConfigService } from '../env-config/env-config.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    MulterModule.registerAsync({
+      useFactory: async (envConfigService: EnvConfigService) => ({
+        storage: multer.diskStorage({
+          destination: envConfigService.getAvatarConfig().avatarDir,
+          filename: (req, file, callback) => {
+            const spilitedOriginalname = file.originalname.split('.');
+            const fileExtName =
+              spilitedOriginalname[spilitedOriginalname.length - 1];
+            const randomName = Array(32)
+              .fill(null)
+              .map(() => Math.round(Math.random() * 16).toString(16))
+              .join('');
+            callback(null, `${randomName}.${fileExtName}`);
+          },
+        }),
+      }),
+      inject: [EnvConfigService],
+    }),
+  ],
   controllers: [UsersController],
   providers: [...usersProviders, UsersService],
   exports: [UsersService],
