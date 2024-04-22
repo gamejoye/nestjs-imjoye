c54f19895e5954f4643a765f53852f768eb1ee75
gamejoye <gamejoye@gmail.com>
2024-04-22 16:18:54 +0800

    test: 提供Chatroom Repository

--- src/modules/ws-gateway/ws-gateway.providers.ts
+++ src/modules/ws-gateway/ws-gateway.providers.ts
@@ -1,11 +1,21 @@
-import { DATA_SOURCE, USER_REPOSITORY } from 'src/common/constants/providers';
+import {
+  CHATROOM_REPOSITORY,
+  DATA_SOURCE,
+  USER_REPOSITORY,
+} from 'src/common/constants/providers';
 import { DataSource } from 'typeorm';
 import { User } from '../users/entities/user.entity';
+import { Chatroom } from '../chatrooms/entities/chatroom.entity';
 
 export const wsGatewayProviders = [
   {
     provide: USER_REPOSITORY,
     useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
+    inject: [DATA_SOURCE],
+  },
+  {
+    provide: CHATROOM_REPOSITORY,
+    useFactory: (dataSource: DataSource) => dataSource.getRepository(Chatroom),
     inject: [DATA_SOURCE],
   },
 ];
