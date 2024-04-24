152572ab1d0518aefdcf451fa3769c320362937e
gamejoye <gamejoye@gmail.com>
2024-04-24 21:02:27 +0800

    feat(constant): USER_CHATROOM_REPOSITORY常量

--- src/common/constants/providers.ts
+++ src/common/constants/providers.ts
@@ -2,4 +2,5 @@ export const USER_REPOSITORY = 'userRepository';
 export const MESSAGE_REPOSITORY = 'messageRepository';
 export const CHATROOM_REPOSITORY = 'chatroomRepository';
 export const USER_FRIENDSHIP_REPOSITORY = 'userFriendshipRepository';
+export const USER_CHATROOM_REPOSITORY = 'userChatroomRepository';
 export const DATA_SOURCE = 'dataSource';
