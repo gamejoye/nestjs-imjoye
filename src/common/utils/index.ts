5719c22cbd75a470fe63077974d3668b751bf279
gamejoye <gamejoye@gmail.com>
2024-04-24 21:01:39 +0800

    feat(utils): 帮助测试的工具函数

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -2,3 +2,4 @@ export * from './retry';
 export * from './times';
 export * from './mock';
 export * from './logger';
+export * from './test-helper';
---
+++ src/common/utils/test-helper.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+import { IDatabaseConfig } from '../types/base.type';
+import { exec } from 'child_process';
+
+export async function initDatabase(
+  databaseConfig: IDatabaseConfig,
+): Promise<void> {
+  const sqlFilePath = path.join(__dirname, '../../../test/testdb.sql');
+  console.log(sqlFilePath);
+  await new Promise<void>((resolve, reject) => {
+    exec(
+      `mysql -h ${databaseConfig.host} --port ${databaseConfig.port} -u ${databaseConfig.username} -p${databaseConfig.password} ${databaseConfig.database} < ${sqlFilePath};`,
+      (error) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return reject(error);
+        }
+        resolve();
+      },
+    );
+  });
+}
+
+export function getNonExsitingId(existingIds: Array<number>): number {
+  const existingIdsSet = new Set<number>();
+  existingIds.forEach((id) => existingIdsSet.add(id));
+  let mex = 1;
+  while (existingIdsSet.has(mex)) mex++;
+  return mex;
+}
