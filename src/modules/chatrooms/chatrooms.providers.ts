c6a5a7c1b44cbd1d46d5d0275ef7caae4afec8d1
gamejoye <gamejoye@gmail.com>
2024-04-24 21:03:09 +0800

    test(service): 测试所有providers

--- src/modules/chatrooms/chatrooms.providers.ts
+++ src/modules/chatrooms/chatrooms.providers.ts
@@ -1,14 +1,29 @@
 import {
   DATA_SOURCE,
   CHATROOM_REPOSITORY,
+  USER_REPOSITORY,
+  USER_CHATROOM_REPOSITORY,
 } from 'src/common/constants/providers';
 import { DataSource } from 'typeorm';
 import { Chatroom } from './entities/chatroom.entity';
+import { UserChatroom } from './entities/user-chatroom.entity';
+import { User } from '../users/entities/user.entity';
 
 export const chatroomsProviders = [
   {
     provide: CHATROOM_REPOSITORY,
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Chatroom),
+    inject: [DATA_SOURCE],
+  },
+  {
+    provide: USER_CHATROOM_REPOSITORY,
+    useFactory: (dataSource: DataSource) =>
+      dataSource.getRepository(UserChatroom),
+    inject: [DATA_SOURCE],
+  },
+  {
+    provide: USER_REPOSITORY,
+    useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
     inject: [DATA_SOURCE],
   },
 ];
