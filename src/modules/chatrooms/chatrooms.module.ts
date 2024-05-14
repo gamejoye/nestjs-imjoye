fa08ed395786c3e0a4783c403488adeba4a1528f
gamejoye <gamejoye@gmail.com>
2024-05-14 18:32:57 +0800

    feat(module): provider UserChatroomService

--- src/modules/chatrooms/chatrooms.module.ts
+++ src/modules/chatrooms/chatrooms.module.ts
@@ -4,10 +4,11 @@ import { ChatroomsService } from './chatrooms.service';
 import { DatabaseModule } from '../database/database.module';
 import { chatroomsProviders } from './chatrooms.providers';
 import { MessagesModule } from '../messages/messages.module';
+import { UserChatroomService } from './user-chatroom.service';
 
 @Module({
   imports: [DatabaseModule, MessagesModule],
   controllers: [ChatroomsController],
-  providers: [...chatroomsProviders, ChatroomsService],
+  providers: [...chatroomsProviders, ChatroomsService, UserChatroomService],
 })
 export class ChatroomsModule {}
