9a30e3f51a7672eef3e1ccfbaf458a1fc9453ea1
gamejoye <gamejoye@gmail.com>
2024-05-20 18:08:50 +0800

    feat(modules): providers UserChatroom Repo, ChatroomsService

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -4,11 +4,12 @@ import { DatabaseModule } from '../database/database.module';
 import { messagesProviders } from './messages.providers';
 import { MessagesController } from './messages.controller';
 import { WsGatewayModule } from '../ws-gateway/ws-gateway.module';
+import { ChatroomsService } from '../chatrooms/chatrooms.service';
 
 @Module({
   imports: [DatabaseModule, WsGatewayModule],
   controllers: [MessagesController],
-  providers: [...messagesProviders, MessagesService],
+  providers: [...messagesProviders, MessagesService, ChatroomsService],
   exports: [MessagesService],
 })
 export class MessagesModule {}
--- src/modules/messages/messages.providers.ts
+++ src/modules/messages/messages.providers.ts
@@ -2,12 +2,14 @@ import {
   CHATROOM_REPOSITORY,
   DATA_SOURCE,
   MESSAGE_REPOSITORY,
+  USER_CHATROOM_REPOSITORY,
   USER_REPOSITORY,
 } from 'src/common/constants/providers';
 import { DataSource } from 'typeorm';
 import { Message } from './entities/message.entity';
 import { User } from '../users/entities/user.entity';
 import { Chatroom } from '../chatrooms/entities/chatroom.entity';
+import { UserChatroom } from '../chatrooms/entities/user-chatroom.entity';
 
 export const messagesProviders = [
   {
@@ -23,6 +25,12 @@ export const messagesProviders = [
   {
     provide: CHATROOM_REPOSITORY,
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Chatroom),
+    inject: [DATA_SOURCE],
+  },
+  {
+    provide: USER_CHATROOM_REPOSITORY,
+    useFactory: (dataSource: DataSource) =>
+      dataSource.getRepository(UserChatroom),
     inject: [DATA_SOURCE],
   },
 ];
