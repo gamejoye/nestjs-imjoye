5723636e6759ad0d788ed2589061a0cdb1868766
gamejoye <gamejoye@gmail.com>
2024-05-20 18:01:59 +0800

    fix: the getAvatarConfig method forgot the declaration

--- src/modules/env-config/interface/env-config.service.interface.ts
+++ src/modules/env-config/interface/env-config.service.interface.ts
@@ -1,6 +1,11 @@
-import { IDatabaseConfig, IJwtConfig } from 'src/common/types/base.type';
+import {
+  IAvatarConfig,
+  IDatabaseConfig,
+  IJwtConfig,
+} from 'src/common/types/base.type';
 
 export interface IEnvConfigService {
   getDatabaseConfig(): IDatabaseConfig;
   getJwtConfig(): IJwtConfig;
+  getAvatarConfig(): IAvatarConfig;
 }
