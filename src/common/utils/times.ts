ddfd6534808fbe4cdc5ad17628b27ea7e19465c2
gamejoye <gamejoye@gmail.com>
2024-05-20 17:53:09 +0800

    feat(utils): 根据timeMs获取datetime

--- src/common/utils/times.ts
+++ src/common/utils/times.ts
@@ -5,3 +5,7 @@ export const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 export function getCurrentDatetime() {
   return dayjs().format(DATETIME_FORMAT);
 }
+
+export function formatDatetime(timeMs: number) {
+  return dayjs(timeMs).format(DATETIME_FORMAT);
+}
