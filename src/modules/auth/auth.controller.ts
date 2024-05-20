a3e69102770a73b17bc06481092dbcdaffdc8b6a
gamejoye <gamejoye@gmail.com>
2024-05-20 17:56:05 +0800

    refactor(controller): refactor to return vo

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  HttpCode,
   HttpException,
   HttpStatus,
   Post,
@@ -16,6 +17,8 @@ import { User } from '../users/entities/user.entity';
 import { LoginUserRequestDto } from './dto/login.dto';
 import { RegisterUserRequestDto } from './dto/register.dto';
 import { UsersService } from '../users/users.service';
+import { LoginVo } from './vo/login.vo';
+import { UserVo } from '../users/vo/user.vo';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -26,32 +29,47 @@ export class AuthController {
   ) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(LocalGuard)
   @ApiBody({ type: LoginUserRequestDto })
   @ApiOperation({ summary: '用户登录' })
-  @ApiResponse({ status: HttpStatus.OK, description: '登录成功' })
-  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '登录失败' })
-  async login(@GetUser() user: User) {
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '登录成功',
+    type: LoginVo,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '登录失败',
+  })
+  async login(
+    @GetUser() user: User,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    @Body() dto: LoginUserRequestDto,
+  ): Promise<LoginVo> {
     const id = user.id;
     const token = jwt.sign(
       { id },
       this.envConfigService.getJwtConfig().secret,
       { expiresIn: 7 * 24 * 60 },
     );
-    const json = {
+    return {
       id,
       token,
     };
-    return json;
   }
 
   @Post('register')
   @ApiBody({ type: RegisterUserRequestDto })
   @ApiOperation({ summary: '用户注册' })
-  @ApiResponse({ status: HttpStatus.OK, description: '注册成功' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '注册成功',
+    type: UserVo,
+  })
   @ApiResponse({ status: HttpStatus.CONFLICT, description: '用户邮箱已经存在' })
   @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '注册失败' })
-  async register(@Body() registerDto: RegisterUserRequestDto) {
+  async register(@Body() registerDto: RegisterUserRequestDto): Promise<UserVo> {
     const { email, password, username, avatarUrl } = registerDto;
     const existing = await this.usersService.getByEmail(email);
     if (existing) {
@@ -64,6 +82,13 @@ export class AuthController {
       passwordHash,
       avatarUrl,
     );
-    return user;
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      avatarUrl: user.avatarUrl,
+      description: user.description,
+      createTime: user.createTime,
+    };
   }
 }
