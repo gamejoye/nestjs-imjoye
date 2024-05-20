891144d73bb0f4115aa1c773abec959c29f4bc4e
gamejoye <gamejoye@gmail.com>
2024-05-20 17:54:52 +0800

    feat(utils): 测试所需工具函数

--- src/common/utils/test-helper.ts
+++ src/common/utils/test-helper.ts
@@ -1,6 +1,27 @@
 import * as path from 'path';
 import { IDatabaseConfig } from '../types/base.type';
 import { exec } from 'child_process';
+import { Repository } from 'typeorm';
+import { Chatroom } from 'src/modules/chatrooms/entities/chatroom.entity';
+import { User } from 'src/modules/users/entities/user.entity';
+import { UserChatroom } from 'src/modules/chatrooms/entities/user-chatroom.entity';
+import { LoginUserRequestDto } from 'src/modules/auth/dto/login.dto';
+
+export const dataForValidIsNumber = ['not a number', NaN, undefined, null];
+export const usersLoginDto: Array<LoginUserRequestDto> = [
+  {
+    email: 'gamejoye@gmail.com',
+    password: '123456..',
+  },
+  {
+    email: 'mikasa@gmail.com',
+    password: '123456..',
+  },
+  {
+    email: 'sakura@gmail.com',
+    password: '123456..',
+  },
+];
 
 export async function initDatabase(
   databaseConfig: IDatabaseConfig,
@@ -26,4 +47,83 @@ export function getNonExsitingId(existingIds: Array<number>): number {
   let mex = 1;
   while (existingIdsSet.has(mex)) mex++;
   return mex;
+}
+
+export async function getChatroomNonExistingId(
+  repository: Repository<Chatroom>,
+): Promise<number> {
+  let nonExistingId = 1;
+  while (true) {
+    const chatroom = await repository.findOne({
+      where: { id: nonExistingId },
+    });
+    if (!chatroom) break;
+    nonExistingId++;
+  }
+  return nonExistingId;
+}
+
+export async function getUserNonExistingId(
+  repository: Repository<User>,
+): Promise<number> {
+  let nonExistingId = 1;
+  while (true) {
+    const chatroom = await repository.findOne({
+      where: { id: nonExistingId },
+    });
+    if (!chatroom) break;
+    nonExistingId++;
+  }
+  return nonExistingId;
+}
+
+export async function getUserNonExistingEmail(
+  repository: Repository<User>,
+): Promise<string> {
+  const username = 'gamejoye';
+  const suffix = '@gmail.com';
+  let index = 0;
+  while (true) {
+    const user = await repository.findOne({
+      where: { email: username + index + suffix },
+    });
+    if (!user) break;
+    index++;
+  }
+  return username + index + suffix;
+}
+
+export async function getNonExistingUserChatroom(
+  usersRepository: Repository<User>,
+  chatroomsRepository: Repository<Chatroom>,
+  userChatroomsRepository: Repository<UserChatroom>,
+): Promise<Array<{ userId: number; chatroomId: number }>> {
+  const users = await usersRepository.find();
+  const chatrooms = await chatroomsRepository.find();
+  const nonExistingUserChatrooms: Array<{
+    userId: number;
+    chatroomId: number;
+  }> = [];
+  for (const user of users) {
+    for (const chatroom of chatrooms) {
+      const userChatroom = await userChatroomsRepository.findOne({
+        where: { user: { id: user.id }, chatroom: { id: chatroom.id } },
+      });
+      if (!userChatroom) {
+        nonExistingUserChatrooms.push({
+          userId: user.id,
+          chatroomId: chatroom.id,
+        });
+      }
+    }
+  }
+  return nonExistingUserChatrooms;
+}
+
+export async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
 }
