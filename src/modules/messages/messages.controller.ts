0f49dfcbd6b6c14846f02e3125d4eea1a060aa14
gamejoye <gamejoye@gmail.com>
2024-05-20 18:09:06 +0800

    refactor(controller): refactor

--- src/modules/messages/messages.controller.ts
+++ src/modules/messages/messages.controller.ts
@@ -2,63 +2,87 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
   HttpStatus,
   Post,
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Message } from './entities/message.entity';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { MessagesService } from './messages.service';
 import { IAddMessageDto } from './dto/add-message.dto';
 import { WsGatewayService } from '../ws-gateway/ws-gateway.service';
+import { MessageVo } from './vo/message.vo';
+import { transformMessage } from './vo/utils';
+import { GetMessagesByChatroomIdDto } from './dto/get-messages-by-chatroom-id.dto';
+import { JwtGuard } from '../auth/jwt.guard';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import { User } from '../users/entities/user.entity';
+import { ChatroomsService } from '../chatrooms/chatrooms.service';
 
 @ApiTags('messages')
 @Controller('messages')
 export class MessagesController {
   constructor(
     protected readonly messagesService: MessagesService,
+    protected readonly chatroomsService: ChatroomsService,
     protected readonly wsService: WsGatewayService,
   ) {}
   @Get()
-  @UseGuards()
-  @ApiQuery({
-    name: 'room_id',
-    required: true,
-    description: '消息所属聊天室的id',
-    type: Number,
-  })
+  @UseGuards(JwtGuard)
   @ApiOperation({ summary: '根据roomId获取消息' })
   @ApiResponse({
     status: HttpStatus.OK,
     description: '成功获取聊天室消息',
-    type: [Message],
+    type: Array<MessageVo>,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '聊天室不存在',
   })
   @ApiResponse({
     status: HttpStatus.UNAUTHORIZED,
     description: '未认证用户',
   })
   async getMessagesByChatroomId(
-    @Query('room_id') roomId: number,
-  ): Promise<Array<Message>> {
-    return this.messagesService.getByPaging(roomId);
+    @GetUser() user: User,
+    @Query() { room_id: roomId }: GetMessagesByChatroomIdDto,
+  ): Promise<Array<MessageVo>> {
+    const chatroom = await this.chatroomsService.getByChatroomId(
+      user.id,
+      roomId,
+    );
+    if (!chatroom) {
+      throw new HttpException('聊天室不存在', HttpStatus.NOT_FOUND);
+    }
+    const messages = await this.messagesService.getByPaging(roomId);
+    return messages.map((message) => transformMessage(message));
   }
 
   @Post()
-  @UseGuards()
+  @UseGuards(JwtGuard)
   @ApiOperation({ summary: '在聊天室中发送消息' })
+  @ApiBody({ type: IAddMessageDto })
   @ApiResponse({
     status: HttpStatus.CREATED,
     description: '成功发送消息',
-    type: Message,
+    type: MessageVo,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '资源不存在',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: '无法在该聊天室发送消息',
   })
   @ApiResponse({
     status: HttpStatus.UNAUTHORIZED,
     description: '未认证用户',
   })
-  async addMessage(@Body() partialMessage: IAddMessageDto): Promise<Message> {
+  async addMessage(@Body() partialMessage: IAddMessageDto): Promise<MessageVo> {
     const message = await this.messagesService.addMessage(partialMessage);
     await this.wsService.notifynChat(message.from.id, message);
-    return message;
+    return transformMessage(message);
   }
 }
