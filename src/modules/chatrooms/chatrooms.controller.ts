0ef55d356492760f176ca974a59d610dac0d7d38
gamejoye <gamejoye@gmail.com>
2024-06-17 22:13:42 +0800

    refactor(controller): fetch summary without visitTime

--- src/modules/chatrooms/chatrooms.controller.ts
+++ src/modules/chatrooms/chatrooms.controller.ts
@@ -1,26 +1,26 @@
 import {
-  Body,
   Controller,
   Get,
-  HttpCode,
   HttpException,
   HttpStatus,
   NotFoundException,
   Param,
   ParseIntPipe,
-  Post,
   Put,
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBody, ApiExtraModels, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { GetChatroomSummariesDto } from './dto/get-chatroom-summaries.dto';
+import {
+  ApiExtraModels,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ChatroomsService } from './chatrooms.service';
 import { GetUser } from '../auth/decorators/get-user.decorator';
 import { User } from '../users/entities/user.entity';
 import { JwtGuard } from '../auth/jwt.guard';
 import { MessagesService } from '../messages/messages.service';
-import { GetChatroomSummaryDto } from './dto/get-chatroom-summary.dto';
 import { GetSingleChatroomDto } from './dto/get-single-chatroom.dto';
 import { Logger } from 'src/common/utils';
 import { UserChatroomService } from './user-chatroom.service';
@@ -70,35 +70,6 @@ export class ChatroomsController {
     }
   }
 
-  @Get(':chatroomId')
-  @UseGuards(JwtGuard)
-  @ApiOperation({ summary: '根据聊天室id获取单个聊天室' })
-  @ApiOkResponseResult({
-    model: ChatroomVo,
-    description: '成功获取单个聊天室',
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: '未认证用户',
-  })
-  @ApiResponse({
-    status: HttpStatus.NOT_FOUND,
-    description: '未找到聊天室',
-  })
-  async getChatroom(
-    @GetUser() user: User,
-    @Param('chatroomId', ParseIntPipe) chatroomId: number,
-  ): Promise<ChatroomVo> {
-    const chatroom = await this.chatroomsService.getByChatroomId(
-      user.id,
-      chatroomId,
-    );
-    if (!chatroom) {
-      throw new NotFoundException('Chatroom not found');
-    }
-    return transformChatroom(chatroom);
-  }
-
   @Get('')
   @UseGuards(JwtGuard)
   @ApiOperation({ summary: '根据userId和friendId获取单个单聊聊天室' })
@@ -128,61 +99,8 @@ export class ChatroomsController {
     return transformChatroom(chatroom);
   }
 
-  @Get('summaries/:chatroomId')
+  @Get('summaries')
   @UseGuards(JwtGuard)
-  @ApiOkResponseResult({
-    model: ChatroomSummaryVo,
-    description: '成功获取单个聊天室的chatroomSummaries',
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: '未认证用户',
-  })
-  async getChatroomSummary(
-    @GetUser() user: User,
-    @Param('chatroomId', ParseIntPipe) chatroomId: number,
-    @Query() { timestamp }: GetChatroomSummaryDto,
-  ): Promise<ChatroomSummaryVo> {
-    const chatroom = await this.chatroomsService.getByChatroomId(
-      user.id,
-      chatroomId,
-    );
-    if (!chatroom) {
-      throw new HttpException(
-        '不存在或无权访问对应聊天室',
-        HttpStatus.NOT_FOUND,
-      );
-    }
-    const userChatroom =
-      await this.userChatroomService.getByUserIdAndChatroomId(
-        user.id,
-        chatroomId,
-      );
-    if (!timestamp) timestamp = userChatroom.latestVisitTime;
-    const unreadMessageCount = await this.messagesService.countByTime(
-      chatroom.id,
-      timestamp,
-    );
-    const latestMessage =
-      await this.messagesService.getLatestMessageByChatroomId(chatroomId);
-    Logger.test('latestMessage: ', latestMessage);
-    const summary: ChatroomSummaryVo = {
-      chatroom: transformChatroom(chatroom),
-      latestVisitTime: userChatroom.latestVisitTime,
-      joinTime: userChatroom.createTime,
-      latestMessage: latestMessage
-        ? transformMessage(latestMessage)
-        : undefined,
-      unreadMessageCount,
-      onlineUserIds: [],
-    };
-    return summary;
-  }
-
-  @Post('summaries')
-  @HttpCode(HttpStatus.OK)
-  @UseGuards(JwtGuard)
-  @ApiBody({ type: GetChatroomSummariesDto })
   @ApiOperation({ summary: '获取聊天室信息概要' })
   @ApiOkResponseResult({
     model: ChatroomSummaryVo,
@@ -195,15 +113,8 @@ export class ChatroomsController {
   })
   async getChatroomSummaries(
     @GetUser() user: User,
-    @Body() getChatroomSummariesDto: GetChatroomSummariesDto,
   ): Promise<Array<ChatroomSummaryVo>> {
     const chatrooms = await this.chatroomsService.getAll(user.id);
-    const timestamps = new Map<number, string>();
-    getChatroomSummariesDto.latestVisitTimes.forEach(
-      ({ id, latestVisitTime }) => {
-        timestamps.set(id, latestVisitTime);
-      },
-    );
 
     const summaries: Array<ChatroomSummaryVo> = await Promise.all(
       chatrooms.map(async (chatroom) => {
@@ -212,8 +123,7 @@ export class ChatroomsController {
             user.id,
             chatroom.id,
           );
-        const timestamp =
-          timestamps.get(chatroom.id) || userChatroom.latestVisitTime;
+        const timestamp = userChatroom.latestVisitTime;
         const latestMessage =
           await this.messagesService.getLatestMessageByChatroomId(chatroom.id);
         const unreadMessageCount = await this.messagesService.countByTime(
@@ -234,5 +144,84 @@ export class ChatroomsController {
     );
     Logger.log(summaries);
     return summaries;
+  }
+
+  @Get(':chatroomId')
+  @UseGuards(JwtGuard)
+  @ApiOperation({ summary: '根据聊天室id获取单个聊天室' })
+  @ApiOkResponseResult({
+    model: ChatroomVo,
+    description: '成功获取单个聊天室',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '未认证用户',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '未找到聊天室',
+  })
+  async getChatroom(
+    @GetUser() user: User,
+    @Param('chatroomId', ParseIntPipe) chatroomId: number,
+  ): Promise<ChatroomVo> {
+    const chatroom = await this.chatroomsService.getByChatroomId(
+      user.id,
+      chatroomId,
+    );
+    if (!chatroom) {
+      throw new NotFoundException('Chatroom not found');
+    }
+    return transformChatroom(chatroom);
+  }
+
+  @Get('summaries/:chatroomId')
+  @UseGuards(JwtGuard)
+  @ApiOkResponseResult({
+    model: ChatroomSummaryVo,
+    description: '成功获取单个聊天室的chatroomSummaries',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '未认证用户',
+  })
+  async getChatroomSummary(
+    @GetUser() user: User,
+    @Param('chatroomId', ParseIntPipe) chatroomId: number,
+  ): Promise<ChatroomSummaryVo> {
+    const chatroom = await this.chatroomsService.getByChatroomId(
+      user.id,
+      chatroomId,
+    );
+    if (!chatroom) {
+      throw new HttpException(
+        '不存在或无权访问对应聊天室',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const userChatroom =
+      await this.userChatroomService.getByUserIdAndChatroomId(
+        user.id,
+        chatroomId,
+      );
+    const timestamp = userChatroom.latestVisitTime;
+    const unreadMessageCount = await this.messagesService.countByTime(
+      chatroom.id,
+      timestamp,
+    );
+    const latestMessage =
+      await this.messagesService.getLatestMessageByChatroomId(chatroomId);
+    Logger.test('latestMessage: ', latestMessage);
+    const summary: ChatroomSummaryVo = {
+      chatroom: transformChatroom(chatroom),
+      latestVisitTime: userChatroom.latestVisitTime,
+      joinTime: userChatroom.createTime,
+      latestMessage: latestMessage
+        ? transformMessage(latestMessage)
+        : undefined,
+      unreadMessageCount,
+      onlineUserIds: [],
+    };
+    return summary;
   }
 }
