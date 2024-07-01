656b437f62086eab45ad59b5ea162ec2452b9750
gamejoye <gamejoye@gmail.com>
2024-07-01 23:26:32 +0800

    feat(constants): FRIEND_REQUEST_REPOSITORY constant

--- src/common/constants/providers.ts
+++ src/common/constants/providers.ts
@@ -2,5 +2,6 @@ export const USER_REPOSITORY = 'userRepository';
 export const MESSAGE_REPOSITORY = 'messageRepository';
 export const CHATROOM_REPOSITORY = 'chatroomRepository';
 export const USER_FRIENDSHIP_REPOSITORY = 'userFriendshipRepository';
+export const FRIEND_REQUEST_REPOSITORY = 'friendRequestRepository';
 export const USER_CHATROOM_REPOSITORY = 'userChatroomRepository';
 export const DATA_SOURCE = 'dataSource';
