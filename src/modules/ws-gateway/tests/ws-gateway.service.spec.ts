005db2c2ffbaf5004c2bfcf7d27b848bf025de9e
gamejoye <gamejoye@gmail.com>
2024-07-02 19:33:22 +0800

    tests(websocket): test notifyNewFriendRequest

--- src/modules/ws-gateway/tests/ws-gateway.service.spec.ts
+++ src/modules/ws-gateway/tests/ws-gateway.service.spec.ts
@@ -9,22 +9,32 @@ import { User } from '../../users/entities/user.entity';
 import { Chatroom } from '../../chatrooms/entities/chatroom.entity';
 import {
   CHATROOM_REPOSITORY,
+  FRIEND_REQUEST_REPOSITORY,
   USER_REPOSITORY,
 } from 'src/common/constants/providers';
 import { ChatroomType } from 'src/common/constants/chatroom';
-import { getCurrentDatetime, initDatabase, mockJwt } from 'src/common/utils';
+import {
+  getCurrentDatetime,
+  getNonExsitingFriendRequest,
+  initDatabase,
+  mockJwt,
+} from 'src/common/utils';
 import { WebSocket } from 'ws';
 import { AUTHORIZATION } from 'src/common/constants/websocketHeaders';
 import { IWebSocketMessage } from 'src/common/types/base.type';
 import { Message } from '../../messages/entities/message.entity';
 import { WebSocketEventType } from 'src/common/constants/websocketEvents';
 import { Logger } from 'src/common/utils';
+import { FriendRequest } from 'src/modules/users/entities/friendrequest.entity';
+import { FriendRequestType } from 'src/common/constants/friendrequest';
 
 describe('WsGatewayService', () => {
   let wsGatewayService: WsGatewayService;
   let usersRepository: Repository<User>;
   let chatroomsRepository: Repository<Chatroom>;
+  let friendRequestsRepository: Repository<FriendRequest>;
   let envConfigService: EnvConfigService;
+  let connectToServer: (users: User[]) => Promise<WebSocket[]>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,6 +47,9 @@ describe('WsGatewayService', () => {
     envConfigService = module.get<EnvConfigService>(EnvConfigService);
     usersRepository = module.get<Repository<User>>(USER_REPOSITORY);
     chatroomsRepository = module.get<Repository<Chatroom>>(CHATROOM_REPOSITORY);
+    friendRequestsRepository = module.get<Repository<FriendRequest>>(
+      FRIEND_REQUEST_REPOSITORY,
+    );
 
     wsGatewayService.onModuleInit();
     await initDatabase(envConfigService.getDatabaseConfig());
@@ -51,6 +64,81 @@ describe('WsGatewayService', () => {
     expect(usersRepository).toBeDefined();
     expect(chatroomsRepository).toBeDefined();
     expect(envConfigService).toBeDefined();
+  });
+
+  connectToServer = async (users: User[]) => {
+    // 1. Mock jwt
+    // 2. user 连接 webSocketServer
+    const userClients = users.map((user) => {
+      const jwtConfig = envConfigService.getJwtConfig();
+      const authorization = mockJwt(user.id, jwtConfig.secret);
+
+      return new WebSocket('ws://localhost:5173', {
+        headers: {
+          [AUTHORIZATION]: authorization,
+        },
+      });
+    });
+
+    const TIMEOUT = 1000;
+
+    // 3. 等待用户全部正确连接
+    try {
+      await new Promise<void>((resolve, reject) => {
+        setTimeout(() => {
+          reject();
+        }, TIMEOUT);
+        let count = 0;
+        userClients.forEach((client) => {
+          client.on('open', () => {
+            count++;
+            if (count === userClients.length) {
+              Logger.test('done!!!');
+              resolve();
+            }
+          });
+          client.on('close', () => {
+            count--;
+          });
+        });
+      });
+    } catch (e) {
+      expect(true).toBe(false);
+    }
+
+    return userClients;
+  };
+
+  it('notifyNewFriendRequest work correctly', async () => {
+    const users = await usersRepository.find();
+    const clients = await connectToServer(users);
+    const expected: Array<[User, FriendRequest]> = [];
+    clients.forEach((client, index) => {
+      client.on('message', (rawData) => {
+        const message: IWebSocketMessage<FriendRequest> = JSON.parse(
+          rawData.toString('utf-8'),
+        );
+        expected.push([users[index], message.payload]);
+      });
+    });
+
+    const ids = getNonExsitingFriendRequest(
+      usersRepository,
+      friendRequestsRepository,
+    );
+    const from = await usersRepository.findOne({ where: { id: ids[0] } });
+    const to = await usersRepository.findOne({ where: { id: ids[1] } });
+    const pendingFq: FriendRequest = {
+      id: 1,
+      from,
+      to,
+      status: FriendRequestType.PENDING,
+      createTime: getCurrentDatetime(),
+      updateTime: null,
+    };
+    await wsGatewayService.notifyNewFriendRequest(to.id, pendingFq);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    expect(expected).toMatchObject([[to, pendingFq]]);
   });
 
   it('notifyChat should work correctly', async () => {
@@ -78,46 +166,8 @@ describe('WsGatewayService', () => {
         .innerJoin('userChatroom.chatroom', 'chatroom')
         .where('chatroom.id = :chatroomId', { chatroomId: multipleChatroom.id })
         .getMany();
-      // 1. Mock jwt
-      // 2. user 连接 webSocketServer
-      const userClients = users.map((user) => {
-        const jwtConfig = envConfigService.getJwtConfig();
-        const authorization = mockJwt(user.id, jwtConfig.secret);
 
-        return new WebSocket('ws://localhost:5173', {
-          headers: {
-            [AUTHORIZATION]: authorization,
-          },
-        });
-      });
-
-      const TIMEOUT = 1000;
-
-      // 3. 等待用户全部正确连接
-      try {
-        await new Promise<void>((resolve, reject) => {
-          setTimeout(() => {
-            reject();
-          }, TIMEOUT);
-
-          let count = 0;
-          userClients.forEach((client) => {
-            client.on('open', () => {
-              count++;
-              Logger.test('open', count, ' total', userClients.length);
-              if (count === userClients.length) {
-                Logger.test('done!!!');
-                resolve();
-              }
-            });
-            client.on('close', () => {
-              count--;
-            });
-          });
-        });
-      } catch (e) {
-        expect(true).toBe(false);
-      }
+      const userClients = await connectToServer(users);
 
       // 4. users[0..x]确认接收消息
       const message: Message = {
@@ -128,14 +178,8 @@ describe('WsGatewayService', () => {
         createTime: getCurrentDatetime(),
       };
       await wsGatewayService.notifynChat(users[0].id, message);
-      let websocketMessages: Array<IWebSocketMessage<Message>>;
-      try {
-        websocketMessages = await new Promise<
-          Array<IWebSocketMessage<Message>>
-        >((resolve, reject) => {
-          setTimeout(() => {
-            reject();
-          }, TIMEOUT);
+      const websocketMessages: Array<IWebSocketMessage<Message>> =
+        await new Promise<Array<IWebSocketMessage<Message>>>((resolve) => {
           const messages: Array<IWebSocketMessage<Message>> = [];
           let count = 0;
           userClients.forEach((client, index) => {
@@ -151,9 +195,6 @@ describe('WsGatewayService', () => {
             });
           });
         });
-      } catch (e) {
-        expect(true).toBe(false);
-      }
 
       // 5. 保证userClient收到的是 NEW_MESSAGE
       expect(websocketMessages.length).toBe(userClients.length);
--- src/modules/ws-gateway/ws-gateway.providers.ts
+++ src/modules/ws-gateway/ws-gateway.providers.ts
@@ -1,11 +1,13 @@
 import {
   CHATROOM_REPOSITORY,
   DATA_SOURCE,
+  FRIEND_REQUEST_REPOSITORY,
   USER_REPOSITORY,
 } from 'src/common/constants/providers';
 import { DataSource } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { Chatroom } from '../chatrooms/entities/chatroom.entity';
+import { FriendRequest } from '../users/entities/friendrequest.entity';
 
 export const wsGatewayProviders = [
   {
@@ -16,6 +18,12 @@ export const wsGatewayProviders = [
   {
     provide: CHATROOM_REPOSITORY,
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Chatroom),
+    inject: [DATA_SOURCE],
+  },
+  {
+    provide: FRIEND_REQUEST_REPOSITORY,
+    useFactory: (dataSource: DataSource) =>
+      dataSource.getRepository(FriendRequest),
     inject: [DATA_SOURCE],
   },
 ];
