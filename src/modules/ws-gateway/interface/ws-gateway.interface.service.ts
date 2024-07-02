f47a11993f7260ea0b8753c0731f163b2a287345
gamejoye <gamejoye@gmail.com>
2024-07-02 19:37:23 +0800

    feat(service): notifyNewFriendRequest

--- src/modules/ws-gateway/interface/ws-gateway.interface.service.ts
+++ src/modules/ws-gateway/interface/ws-gateway.interface.service.ts
@@ -1,7 +1,9 @@
 import { Message } from 'src/modules/messages/entities/message.entity';
+import { FriendRequest } from 'src/modules/users/entities/friendrequest.entity';
 // import { WebSocket } from 'ws';
 
 export interface IWsGatewayService {
   // handleOnPing(client: WebSocket, message: string): Promise<void>;
   notifynChat(from: number, message: Message): Promise<void>;
+  notifyNewFriendRequest(to: number, fq: FriendRequest): Promise<void>;
 }
