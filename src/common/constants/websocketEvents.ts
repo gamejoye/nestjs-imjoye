146061ca91a6af496d38af8a059f277b4c8b0514
gamejoye <gamejoye@gmail.com>
2024-07-02 19:33:39 +0800

    feat(constants): NEW_FRIEND_REQUEST

--- src/common/constants/websocketEvents.ts
+++ src/common/constants/websocketEvents.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
 export class WebSocketEventType {
-  static NEW_MESSAGE = 1;     // 新消息
+  static NEW_MESSAGE = 1;         // 新消息
+  static NEW_FRIEND_REQUEST = 2;  // 新的好友请求
 }
