3c75afa0b353ab984eb459246e54482471b59e19
gamejoye <gamejoye@gmail.com>
2024-07-28 01:19:41 +0800

    feat(constants): FRIEND_REQUEST

--- src/common/constants/database-tables.ts
+++ src/common/constants/database-tables.ts
@@ -4,3 +4,4 @@ export const USER = 'user';                           // 用户
 export const CHATROOM = 'chatroom';                   // 聊天室
 export const USER_CHATROOM = 'user_chatroom';         // 用户聊天室映射表
 export const USER_FRIENDSHIP = 'user_friendship';     // 用户好友映射表
+export const FRIEND_REQUEST = 'friend_request';       // 好友请求表
