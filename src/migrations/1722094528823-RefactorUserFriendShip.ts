7b862bf3f49b7350c7bb97fbb02722c2a3a828aa
gamejoye <gamejoye@gmail.com>
2024-07-28 01:20:13 +0800

    perf: refactor UserFriendShip Table migration

---
+++ src/migrations/1722094528823-RefactorUserFriendShip.ts
@@ -0,0 +1,36 @@
+import { USER_FRIENDSHIP } from 'src/common/constants/database-tables';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class RefactorUserFriendShip1722094528823 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // 删除 `updateTime` 列
+    await queryRunner.dropColumn(USER_FRIENDSHIP, 'updateTime');
+
+    // 删除 `status` 列
+    await queryRunner.dropColumn(USER_FRIENDSHIP, 'status');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // 恢复 `updateTime` 列
+    await queryRunner.addColumn(
+      USER_FRIENDSHIP,
+      new TableColumn({
+        name: 'update_time',
+        type: 'datetime',
+        isNullable: true,
+        default: null,
+      }),
+    );
+
+    // 恢复 `status` 列
+    await queryRunner.addColumn(
+      USER_FRIENDSHIP,
+      new TableColumn({
+        name: 'status',
+        type: 'enum',
+        enum: ['PENDING', 'REJECT', 'ACCEPT'],
+        default: "'PENDING'",
+      }),
+    );
+  }
+}
