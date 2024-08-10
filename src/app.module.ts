e50ec6d2193870f5fc1ffaa00eaf6a3c6c7dfa1e
gamejoye <gamejoye@gmail.com>
2024-08-11 01:46:09 +0800

    feat(modules): Redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,11 @@ import { ChatroomsModule } from './modules/chatrooms/chatrooms.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { WsGatewayModule } from './modules/ws-gateway/ws-gateway.module';
 import { LoggerMiddleWare } from './common/middlewares/logger.middleware';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -15,12 +18,32 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: redisStore,
+          host: configService.get('REDIS_HOST') || 'localhost',
+          pport: +(configService.get('REDIS_PORT') || '6379'),
+          db: 0,
+        };
+      },
+    }),
     UsersModule,
     AuthModule,
     ChatroomsModule,
     MessagesModule,
     WsGatewayModule,
   ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
+  exports: [CacheModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
