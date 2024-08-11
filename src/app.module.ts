273431954cefaf13f92f55d5b6e61e3ec4180797
gamejoye <gamejoye@gmail.com>
2024-08-11 13:59:34 +0800

    feat: setup CacheModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MessagesModule } from './modules/messages/messages.module';
 import { WsGatewayModule } from './modules/ws-gateway/ws-gateway.module';
 import { LoggerMiddleWare } from './common/middlewares/logger.middleware';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import configuration from './config/configuration';
+import configuration, { Config } from './config/configuration';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
 import { APP_INTERCEPTOR } from '@nestjs/core';
@@ -23,11 +23,12 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
+        const redisC = configService.get<Config['redis']>('redis');
         return {
           store: redisStore,
-          host: configService.get('REDIS_HOST') || 'localhost',
-          pport: +(configService.get('REDIS_PORT') || '6379'),
-          db: 0,
+          host: redisC.host,
+          port: redisC.port,
+          db: redisC.db,
         };
       },
     }),
